4e1d83e15a4fef6728add84095ac3b90fd598d80
Paaaaris <93464048+Paaaaris@users.noreply.github.com>
2021-10-31 14:37:23 +0800

    Add files via upload

---
+++ product.php
@@ -0,0 +1,44 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h3 class="text-uppercase text-white font-weight-bold">All Products</h3>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+   <section class="page-section" id="menu">
+        <div id="menu-field" class="card-deck">
+                <?php 
+                    include'admin/db_connect.php';
+                    $qry = $conn->query("SELECT * FROM  product_list order by rand() ");
+                    while($row = $qry->fetch_assoc()):
+                    ?>
+                    <div class="col-lg-3">
+                     <div class="card menu-item ">
+                        <img src="assets/img/<?php echo $row['img_path'] ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title"><?php echo $row['name'] ?></h5>
+                          <p class="card-text truncate"><?php echo $row['description'] ?></p>
+                          <div class="text-center">
+                              <button class="btn btn-sm btn-outline-primary view_prod btn-block" data-id=<?php echo $row['id'] ?>><i class="fa fa-eye"></i> Add to Cart</button>
+                              
+                          </div>
+                        </div>
+                        
+                      </div>
+                      </div>
+                    <?php endwhile; ?>
+        </div>
+    </section>
+    <script>
+        
+        $('.view_prod').click(function(){
+            uni_modal_right('Product','view_prod.php?id='+$(this).attr('data-id'))
+        })
+    </script>
+    
